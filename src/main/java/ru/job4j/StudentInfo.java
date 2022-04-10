e709a6364bdcdde5f7fccf69bcfa58e0dee91c4a
erikmarino <eriksun86@mail.ru>
2022-04-10 10:33:26 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class StudentInfo {
         public static void main(String[] args) {
             System.out.println("Erik Marino");
-            //System.out.println("04.09.1986");
+            System.out.println("04.09.1986");
     }
 }
