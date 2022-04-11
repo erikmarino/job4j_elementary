ea6144c7bcc7da75c9da6fd8fdbc05d2e55cbc10
erikmarino <eriksun86@mail.ru>
2022-04-11 18:20:33 +0300

    4.1.2. Глупый бот [#31323]

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -1,14 +1,14 @@
 package ru.job4j.calculator;
 
 public class ArgMethod {
-    public static void hello(String name, int age) { //объявление метода hello (указываем параметры, и тип параметра)
+    public static void hello(String name, int age) {
         System.out.println("Hello, " + name + ", age = " + age);
     }
 
-    public static void main(String[] args) { // объявление метода мэйн, стринг тип данных, аргс имя)
-        String name = "Job4j"; // объявление переменной нэйм типа СТРИНГ
-        int age = 6; // объявление переменной эге типа инт
-        ArgMethod.hello(name, age); // вызов метода hello
+    public static void main(String[] args) {
+        String name = "Job4j";
+        int age = 6;
+        ArgMethod.hello(name, age);
         ArgMethod.hello(name, age);
         ArgMethod.hello(name, age);
         ArgMethod.hello(name, age);
