13d98132be548c386432273e3c32139b99355373
erikmarino <eriksun86@mail.ru>
2022-04-11 18:05:59 +0300

    1

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -1,14 +1,14 @@
 package ru.job4j.calculator;
 
 public class ArgMethod {
-    public static void hello(String name, int age) {
+    public static void hello(String name, int age) { //объявление метода hello (указываем параметры, и тип параметра)
         System.out.println("Hello, " + name + ", age = " + age);
     }
 
-    public static void main(String[] args) {
-        String name = "Job4j";
-        int age = 6;
-        ArgMethod.hello(name, age);
+    public static void main(String[] args) { // объявление метода мэйн, стринг тип данных, аргс имя)
+        String name = "Job4j"; // объявление переменной нэйм типа СТРИНГ
+        int age = 6; // объявление переменной эге типа инт
+        ArgMethod.hello(name, age); // вызов метода hello
         ArgMethod.hello(name, age);
         ArgMethod.hello(name, age);
         ArgMethod.hello(name, age);
