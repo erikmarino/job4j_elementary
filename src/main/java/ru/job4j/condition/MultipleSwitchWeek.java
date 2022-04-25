2e688e51481e2190240d6d4257e96fcb2265ae28
erikmarino <eriksun86@mail.ru>
2022-04-25 10:56:48 +0300

    1

--- src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class MultipleSwitchWeek {
     public static int numberOfDay(String name) {
-        return switch (name) {
+        return  switch (name) {
             case "Понедельник", "Monday" -> 1;
             case "Вторник", "Tuesday" -> 2;
             case "Среда", "Wednesday" -> 3;
