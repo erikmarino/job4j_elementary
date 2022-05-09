71d5b841a26579c11dd336f5d30d6fd4639cb599
erikmarino <eriksun86@mail.ru>
2022-05-09 17:26:39 +0300

    2.1. Рефакторинг - Расстояние между точками.

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,45 +7,37 @@ public class PointTest {
 
     @Test
     public void when00to20then2() {
-        double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        double expected = 2.0;
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when12to34then2dot82() {
         double expected = 2.82;
-        int x1 = 1;
-        int y1 = 2;
-        int x2 = 3;
-        int y2 = 4;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(1, 2);
+        Point b = new Point(3, 4);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when28to76then5dot38() {
         double expected = 5.38;
-        int x1 = 2;
-        int y1 = 8;
-        int x2 = 7;
-        int y2 = 6;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(2, 8);
+        Point b = new Point(7, 6);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when99to33then8dot48() {
         double expected = 8.48;
-        int x1 = 9;
-        int y1 = 9;
-        int x2 = 3;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(9, 9);
+        Point b = new Point(3, 3);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 }
